7018a8a294eccd7c4b2835cb77c50698ebcbdecd
epiphronquant <91112822+epiphronquant@users.noreply.github.com>
2021-10-07 14:27:33 +0800

    Update Yahoo_Downloader.py

--- Yahoo_Downloader.py
+++ Yahoo_Downloader.py
@@ -75,19 +75,19 @@ with column_1:### ### Download Statements chart
     e = e.T.reset_index(drop=True).T
     st.dataframe(e)
     ### Adds the option to download data
-    def to_excel(df, index):
+    def to_excel(df, header):
         output = BytesIO()
         writer = pd.ExcelWriter(output, engine='xlsxwriter')
-        df.to_excel(writer, sheet_name='Sheet1', index = index)
+        df.to_excel(writer, sheet_name='Sheet1', header = header)
         writer.save()
         processed_data = output.getvalue()
         return processed_data
-    def get_table_download_link(df, index):
+    def get_table_download_link(df, header):
         """Generates a link allowing the data in a given panda dataframe to be downloaded
         in:  dataframe
         out: href string
         """
-        val = to_excel(df, index)
+        val = to_excel(df, header)
         b64 = base64.b64encode(val)  # val looks like b'...'
         return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="extract.xlsx">Download xlsx</a>' # decode b'abc' => abc    
     st.markdown(get_table_download_link(output, False), unsafe_allow_html=True)
